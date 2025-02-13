d8cffeea25d367578dcd9feaec08aa554e0e276a
robertthown <robertomello404@gmail.com>
2025-02-13 09:55:09 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,10 +11,10 @@ describe('Account Mongo Repository', () => {
         await MongoHelper.disconnect()
     })
 
-    // beforeEach(async () => {
-    //     const accountCollection = MongoHelper.getCollection('accounts')
-    //     await accountCollection.deleteMany({})
-    //   })   
+    beforeEach(async () => {
+        const accountCollection = MongoHelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
+      })   
     
     const makeSut = (): AccountMongoRepository => {
         return new AccountMongoRepository()
